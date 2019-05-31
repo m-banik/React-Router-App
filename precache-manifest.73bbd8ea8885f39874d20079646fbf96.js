6e85bf7653777ed2f573074fc05903b013726676
Marcin Banik <mrn.banik@gmail.com>
2019-05-31 02:47:19 +0200

    Updates

---
+++ precache-manifest.73bbd8ea8885f39874d20079646fbf96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9a85d4677268fbfa475",
+    "url": "/React-Router-App/static/css/main.c94da27c.chunk.css"
+  },
+  {
+    "revision": "d9a85d4677268fbfa475",
+    "url": "/React-Router-App/static/js/main.d9a85d46.chunk.js"
+  },
+  {
+    "revision": "9de863c1243eb0838495",
+    "url": "/React-Router-App/static/js/1.9de863c1.chunk.js"
+  },
+  {
+    "revision": "cf063d2f072aefc1f291",
+    "url": "/React-Router-App/static/js/runtime~main.cf063d2f.js"
+  },
+  {
+    "revision": "cc427397bc973c60d31f838933af196d",
+    "url": "/React-Router-App/static/media/header1.cc427397.jpg"
+  },
+  {
+    "revision": "f2d835d4fc9f51c5f2045274686b5846",
+    "url": "/React-Router-App/static/media/header2.f2d835d4.jpg"
+  },
+  {
+    "revision": "cb269d029f1e08b6684b37a258c7c067",
+    "url": "/React-Router-App/static/media/header3.cb269d02.jpg"
+  },
+  {
+    "revision": "2f663c6237ed075c95448b9f6f6e17a2",
+    "url": "/React-Router-App/index.html"
+  }
+];
